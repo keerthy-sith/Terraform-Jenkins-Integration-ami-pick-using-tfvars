991040220889bac08c9ea408d0eac0683fa11c4b
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-18 13:58:31 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,6 +1,15 @@
 variable "instance_type" {
+  description = "AMI ID"
+  type        = string
+  default     = "t2.nano"
 }
 variable "ec2_ami" {
+  description = "AMI ID"
+  type        = string
+  default     = ""
 }
 variable "instance_name"{
+  type        = string
+  default     = "windows"
+  
 }
