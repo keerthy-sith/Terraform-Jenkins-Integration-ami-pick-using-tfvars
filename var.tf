2cb05bb9f16984ae298bef9b56cfaa366cfff396
keerthy-sith <96583397+keerthy-sith@users.noreply.github.com>
2022-01-18 13:57:14 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,15 +1,15 @@
 variable "instance_type" {
   description = "AMI ID"
   type        = string
-  default     = ""
+  default     = "t2.micro"
 }
 variable "ec2_ami" {
   description = "AMI ID"
   type        = string
-  default     = ""
+  default     = "ami-08e4e35cccc6189f4"
 }
 variable "instance_name"{
   type        = string
-  default     = ""
+  default     = "linux"
   
 }
